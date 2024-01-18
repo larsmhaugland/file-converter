9bc2e63829b625d4b93278386883e003ce483d33
Lars Martin Haugland <larmha@stud.ntnu.no>
2024-01-18 10:10:41 +0100

    Logger singleton

--- Logger.cs
+++ Logger.cs
@@ -2,10 +2,29 @@
 
 public class Logger
 {
+	private static Logger instance;
+	private static readonly object lockObject = new object();
 	string fileName;		// Path to log file
 
-	public Logger()
+	private Logger()
 	{
+	}
+	public static Logger Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				lock (lockObject)
+				{
+					if (instance == null)
+					{
+                        instance = new Logger();
+                    }
+				}
+			}
+			return instance;
+		}
 	}
 
 	private void writeLog(string message)
