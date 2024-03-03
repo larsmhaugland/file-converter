68eea0f38d064b06c831be6f707be3b6ffe2df2a
seigmann <aurora@skomsvold.net>
2024-03-03 16:05:15 +0100

    bug fix

--- LinuxSpecifics/LinuxSetup.cs
+++ LinuxSpecifics/LinuxSetup.cs
@@ -86,7 +86,7 @@ class LinuxSetup
         { 
             ProcessStartInfo startInfoArch = new ProcessStartInfo();
             startInfoArch.FileName = "/bin/bash";
-            startInfoArch.Arguments = $"git clone https://aur.archlinux.org/siegfried.git && cd siegfried && makepkg -si";
+            startInfoArch.Arguments = $"-c \"git clone https://aur.archlinux.org/siegfried.git && cd siegfried && makepkg -si\"";
             Process processArch = new Process();
             processArch.StartInfo = startInfoArch;
             processArch.Start();
