12980ace2601510b43f9357b3cc294508c918e5f
Lars Martin Haugland <larmha@stud.ntnu.no>
2024-03-18 13:20:31 +0100

    merge

--- src/Main.cs
+++ src/Main.cs
@@ -54,6 +54,134 @@ class Program
 		if (!OperatingSystem.IsLinux())
 		{
             Directory.SetCurrentDirectory("../../../");
+<<<<<<< HEAD
+        }
+		Settings settings = Settings.Instance;
+		Console.WriteLine("Reading settings...");
+		settings.ReadSettings("./Settings.xml");
+        settings.SetUpFolderOverride("./Settings.xml");
+        Logger logger = Logger.Instance;
+
+		FileManager fileManager = FileManager.Instance;
+        Siegfried sf = Siegfried.Instance;
+		
+        //TODO: Check for malicous input files
+        try
+		{
+			//Check if user wants to use files from previous run
+			sf.AskReadFiles();
+			//Check if files were added from previous run
+			if (!sf.Files.IsEmpty)
+			{
+				//Import files from previous run
+				Console.WriteLine("Checking files from previous run...");
+				fileManager.ImportFiles(sf.Files.ToList());
+				var compressedFiles = sf.IdentifyCompressedFilesJSON(GlobalVariables.parsedOptions.Input);
+				fileManager.ImportCompressedFiles(compressedFiles);
+			}
+			else
+			{
+				Console.WriteLine("Copying files from {0} to {1}...",GlobalVariables.parsedOptions.Input, GlobalVariables.parsedOptions.Output);
+				//Copy files
+				sf.CopyFiles(GlobalVariables.parsedOptions.Input, GlobalVariables.parsedOptions.Output);
+				Console.WriteLine("Identifying files...");
+				//Identify and unpack files
+				fileManager.IdentifyFiles();
+			}
+		} catch (Exception e)
+		{
+			Console.WriteLine("[FATAL] Could not identify files: " + e.Message);
+			logger.SetUpRunTimeLogMessage("Main: Error when copying/unpacking/identifying files: " + e.Message, true);
+			return;
+		}
+		ConversionManager cm = ConversionManager.Instance;
+		
+
+		if (fileManager.Files.Count > 0)
+		{			
+			string input;
+			do
+			{
+                logger.AskAboutReqAndConv();
+                fileManager.DisplayFileList();
+				var oldColor = Console.ForegroundColor;
+				Console.ForegroundColor = ConsoleColor.Blue;
+				Console.WriteLine("Requester: {0}",Logger.JsonRoot.requester);
+				Console.WriteLine("Converter: {0}",Logger.JsonRoot.converter);
+				Console.WriteLine("MaxThreads: {0}", GlobalVariables.maxThreads);
+				Console.WriteLine("Timeout in minutes: {0}", GlobalVariables.timeout);
+				Console.ForegroundColor = oldColor;
+                Console.Write("Do you want to proceed with these settings (Y (Yes) / A (Abort) / R (Reload) / G (Change in GUI): ");
+                string ?r = Console.ReadLine();
+				r = r?.ToUpper() ?? " ";
+				input = r;
+                if (input == "R")
+                {
+                    Console.WriteLine("Change settings file and hit enter when finished (Remember to save file)");
+                    Console.ReadLine();
+                    settings.ReadSettings("./Settings.xml");
+                    settings.SetUpFolderOverride("./Settings.xml");
+                }
+				if (input == "G")
+				{
+					//TODO: Start GUI
+					Console.WriteLine("Not implemented yet...");
+                    settings.ReadSettings("./Settings.xml");
+                    settings.SetUpFolderOverride("./Settings.xml");
+                }
+            } while (input != "Y" && input != "A");
+			if (input == "A")
+			{
+                return;
+            }
+            Console.WriteLine("Converting files...");
+
+            
+            try
+			{
+                cm.ConvertFiles();
+				//Delete siegfrieds json files
+				sf.ClearOutputFolder();
+			} catch (Exception e)
+			{
+                Console.WriteLine("Error while converting " + e.Message);
+                logger.SetUpRunTimeLogMessage("Main: Error when converting files: " + e.Message, true);
+			}
+			finally
+			{
+				Console.WriteLine("Conversion finished:");
+				fileManager.DisplayFileList();
+                Console.WriteLine("Documenting conversion...");
+                fileManager.DocumentFiles();
+			}
+			Console.WriteLine("Compressing folders...");
+			sf.CompressFolders();
+		}
+	}
+    static void MaximizeAndCenterConsoleWindow()
+    {
+		//Only maximize and center the console window if the OS is Windows
+		if (Environment.OSVersion.Platform != PlatformID.Win32NT)
+		{
+			return;
+		}
+        int screenWidth = Console.LargestWindowWidth;
+        int screenHeight = Console.LargestWindowHeight;
+
+        int windowWidth = screenWidth ;  // You can adjust this as needed
+        int windowHeight = screenHeight; // You can adjust this as needed
+
+        Console.SetWindowSize(windowWidth, windowHeight);
+        Console.BufferHeight = windowHeight;
+        Console.BufferWidth = windowWidth;
+
+        int left = Math.Max((screenWidth - windowWidth) / 2, 0);
+        int top = Math.Max((screenHeight - windowHeight) / 2, 0);
+
+        //Console.SetWindowPosition(left, top);
+    }
+}
+=======
         }
 		else
 		{
@@ -185,3 +313,4 @@ class Program
         //Console.SetWindowPosition(left, top);
     }
 }
+>>>>>>> 9aa36dc57225f4e0cf628aa1732f6edef08b3f73
