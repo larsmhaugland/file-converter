9aa36dc57225f4e0cf628aa1732f6edef08b3f73
Philip <philiasu@stud.ntnu.no>
2024-03-18 11:14:09 +0100

    small changes

--- GUI/ChangeConverterSettings/ChangeConverterSettings/obj/Debug/net8.0/ChangeConverterSettings.AssemblyInfo.cs
+++ GUI/ChangeConverterSettings/ChangeConverterSettings/obj/Debug/net8.0/ChangeConverterSettings.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChangeConverterSettings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16f6d0f0ade6aa1385727661916ee23f23f0bdbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+980d90c081eb13af63ac35311c11afa04cf50be8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChangeConverterSettings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChangeConverterSettings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
